5d0b64e9e61aa99746c2d44a91c10e786ee82f57
YurecUA <81627173+YurecUA@users.noreply.github.com>
2022-11-09 04:19:45 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Функция, которая деструктуризирует объект и выдает разметку для вставки изображений
+const makeImagesMarkup = function (galleryItems) {
+  const { original, preview, description } = galleryItems;
+  return `
+			<div class="gallery__item">
+  			<a class="gallery__link" 
+				href="${original}">
+    			<img
+      			class="gallery__image"
+      			src="${preview}"
+      			data-source="${original}"
+      			alt="${description}"
+    			/>
+  			</a>
+			</div>`;
+};
+
+// Определяю куда будет вставлена разметка, функцией прогоняю весь массив объектов
+// для создания разметки и заливаю полученный текст в HTML
+const galleryListHeaderEl = document.querySelector('.gallery');
+const galleryListMarkup = galleryItems.map(makeImagesMarkup).join('');
+
+galleryListHeaderEl.insertAdjacentHTML('beforeend', galleryListMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
